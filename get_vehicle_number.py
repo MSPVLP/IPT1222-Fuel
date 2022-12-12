4b835ac3bbd90177927f23d50b2ab02f80d57dfc
chandrika <student7.ce@mspvl.com>
2022-12-12 12:02:18 +0530

    get vehicle number

--- get_vehicle_number.py
+++ get_vehicle_number.py
@@ -5,11 +5,10 @@ def get_vehicle_number():
     This function get the vehicle number from user
     return:It produces the veh_no
     """
-    veh_no = input()
+    veh_no = input("Enter Your Vehicle Number:")
     return veh_no
 
 
 if __name__ == "__main__":
-    print("Enter Your Vehicle Number:")
     vno = get_vehicle_number()
     print("Your Vehicle Number is:", vno)
